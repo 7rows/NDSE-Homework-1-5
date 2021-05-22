56da13b6eb33184e2630f439b6aa15078ae5e60f
nikolay <fedorov.nvas@gazprom-neft.ru>
2021-05-22 20:32:26 +0300

    delete config

--- config.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config()
-const ENDPOINT = 'http://api.weatherstack.com'
-const KEY = process.env.API_WEATHER_KEY
-module.exports = {
-    URL:`${ENDPOINT}/current?access_key=${KEY}&units=m`
-}
